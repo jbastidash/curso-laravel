d187042ddd6e64f23f2c5a032825a8440b93e115
cursos <jbastidash@gmail.com>
2019-08-09 19:36:09 -0400

    Seeders y model factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->truncateTablas([
+            'rol','permiso'
+        ]);
+        $this->call(TablaRolSeeder::class);
+        $this->call(TablaPermisoSeeder::class);
     }
+
+    public function truncateTablas (array $tablas)
+    {
+     //   DB::statement('SET_FOREING_KEY_CHECK=0;');
+        Foreach ($tablas as $tabla){
+            DB::table($tabla)->truncate();
+        }
+       // DB::statement('SET_FOREING_KEY_CHECK=1;');
+
+    }
+
 }
---
+++ database/seeds/TablaRolSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+
+class TablaRolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+$rol=[
+    'administrador',
+    'editor',
+    'supervisor'
+];
+
+ foreach ($rol as $key=>$value){
+     DB::table('rol')->Insert(['nombre'=>$value,
+     'created_at'=> Carbon::now()->format('Y-m-d H:i:s')
+     ]);
+
+ }
+    }
+
+    
+}
